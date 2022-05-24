394b50caacb8335732a897b39f65f2f626c69110
PabloFerrari <pablo.ferrari019@gmail.com>
2022-05-24 12:27:22 -0300

    local

--- javascript/main.js
+++ javascript/main.js
@@ -2,12 +2,15 @@ const buscador = document.querySelector('#buscador');
 const boton = document.getElementById('boton');
 const mostrar = document.getElementById('mostrar');
 
+
+
 const paises = async()=>{
     const responsepaises = await fetch("/javascript/jsons/main.json")
     const pais = await responsepaises.json();
     
     function buscarPais(){
-
+        
+       
         pais.forEach((paises)=>{
         
             const filtrarPais = ()=>{
@@ -24,10 +27,59 @@ const paises = async()=>{
                         <li>${paisbuscar.nombre}</li>
                         <li>valor: $${paisbuscar.valor}  </li>
                         <li><img src="${paisbuscar.imagen}" class="chico"></li>
-                        <a href="#" class="botonCompra">Comprar</a>
+                        <a href="#" class="botonCompra" id="botonCompra">Comprar</a>
                         </ul>
                         </div>
                         `             
+
+
+                        const botonCompra = document.getElementById('botonCompra');
+
+                        class recojerYmostrarpais {
+
+                            constructor(paiselejido){
+                                this.pais = paiselejido
+                            }
+
+                            mostrarDatosenInput(){
+                                const paisSelecct = document.querySelector('#paisSelecct');
+                                paisSelecct.textContent = "";
+                            };
+                        }
+
+                        class recojerYmostrarpaislocal{
+
+                            constructor(paises){
+                                this.pais = paises;
+                                // this.email = email;
+                            }
+                            
+                            pedirdatos(){
+                                const mostrarco = document.getElementById('mostrarPais');
+                                const mostrarpais = document.createElement('div');
+                                mostrarpais.textContent = "Tu seleccion fue" + this.pais;
+                                mostrarco.appendChild(mostrarpais)
+                            };
+                        };
+                        
+
+                        botonCompra.addEventListener('click',()=>{
+                            const cotizador = document.querySelector('#cotizador');
+                            cotizador.className = 'duplicar';
+
+                            let pais = [];
+                            let infoPais = localStorage.getItem('paises');
+
+                            const paisseleccionado = buscador.value.toLowerCase();
+                            pais.push(paisseleccionado);
+                            localStorage.setItem('paises', JSON.stringify(paisseleccionado))
+                        
+
+                            const mostrarStorage = JSON.parse( localStorage.getItem('paises'));
+                            const nuevoviaje = new recojerYmostrarpaislocal(mostrarStorage);
+                            nuevoviaje.pedirdatos();
+
+                        });
                     }
                 }
                 if(mostrar.innerHTML === ''){
@@ -50,6 +102,6 @@ const paises = async()=>{
     }
         boton.addEventListener('click', buscarPais)
 
-}
-
-paises();
+    }
+    
+    paises();
