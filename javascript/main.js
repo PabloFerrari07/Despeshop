4acb27fd384861588a14132a7b0eebed078298ed
PabloFerrari <pablo.ferrari019@gmail.com>
2022-05-30 12:30:13 -0300

    fin

--- javascript/main.js
+++ javascript/main.js
@@ -45,17 +45,19 @@ const paises = async()=>{
                             
                             pedirdatos(){
                                 const mostrarco = document.getElementById('mostrarPais');
-                                const mostrarpais = document.createElement('div');
+                                // const mostrarpais = document.createElement('div');
 
-                                mostrarpais.className = "Modificar_div";
-                                mostrarpais.textContent = "Asique vas a " + this.pais;
-                                mostrarco.appendChild(mostrarpais);
-                         
-                                if(mostrarco.contains(mostrarpais) ){
+                                // mostrarpais.className = "Modificar_div";
+                                // mostrarpais.textContent = "Asique vas a " + this.pais;
+                                // mostrarco.appendChild(mostrarpais);
+                                mostrarco.innerHTML = `
+                                <input type="text" id="viaje" placeholder="${this.pais}" disabled="" ">
+                                `
+                                // if(mostrarco.contains(input) ){
 
-                                    setTimeout(()=>{
-                                        mostrarpais.textContent = '';
-                                       },3000);
+                                //     setTimeout(()=>{
+                                //         mostrarpais.textContent = '';
+                                //        },3000);
 
                                        const mostrarImagen = document.getElementById('cargarImg');
                                        mostrarImagen.innerHTML = `
@@ -66,7 +68,7 @@ const paises = async()=>{
                                        <li class="liimg"><img src="${paisbuscar.imagen}" class="chico"></li>
                                        </ul>
                                        `
-                                }
+                                
                  
                             };
 
@@ -90,18 +92,33 @@ const paises = async()=>{
                         function realizarComprar(){
                             const precio = paisbuscar.valor;
                             if(precio === paisbuscar.valor){
-                                // const precioFinal = precio/opcion.value;
-                                // alert(precioFinal.value)
+                           
                                 const select = document.getElementById('seleccion');
                                 const perciofinal =parseInt(precio/select.value);
                                 const tarjeta = document.getElementById('tarjeta');
 
-                                Swal.fire({
-                                    title: 'Su compra fue realizada!',
-                                    text: 'Su precio final es de $'+perciofinal + ' al mes' + ' con tu tarjeta: ' + tarjeta.value,
-                                    icon: 'success',
-                                    confirmButtonText: 'Cool'
-                                  })
+                               switch(tarjeta.value){
+                                   case 'visa':
+                                       Swal.fire({
+                                           title: 'Su compra fue realizada!',
+                                           text: 'Su precio final es de $'+perciofinal + ' al mes' + ' con tu tarjeta: ' + tarjeta.value,
+                                           icon: 'success',
+                                           confirmButtonText: 'Viajar!'
+                                         })
+                                         break;
+                                  case 'mastercard':
+                                       Swal.fire({
+                                           title: 'Su compra fue realizada!',
+                                           text: 'Su precio final es de $'+perciofinal + ' al mes' + ' con tu tarjeta: ' + tarjeta.value,
+                                           icon: 'success',
+                                           confirmButtonText: 'Viajar!'
+                                         })
+                                         break;         
+                                         
+                                         
+                                        }
+                              
+
                             }
                         }
                         
@@ -109,10 +126,11 @@ const paises = async()=>{
                         botonCompra.addEventListener('click',()=>{
               
                             const cotizador = document.querySelector('#cotizador');
+                    
                             cotizador.className = 'duplicar';
 
                             let pais = [];
-                            let infoPais = localStorage.getItem('paises');
+                            // let infoPais = localStorage.getItem('paises');
 
                             const paisseleccionado = buscador.value.toLowerCase();
                             pais.push(paisseleccionado);
